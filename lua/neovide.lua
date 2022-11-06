d595f4b91af995acc38e0a19aebb37f24a8e5402
Geordie Powers <gp@gpow.ca>
2022-11-06 18:17:15 -0500

    neovide high refresh rate

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -4,4 +4,5 @@ if not vim.g.neovide then
 else
   vim.opt.guifont = "JetBrains Mono:h12"
   vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_refresh_rate = 120 -- only has an effect on high refresh displays
 end
