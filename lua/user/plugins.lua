ca196e8e259463012a9f19fd49a59d6069329efc
Geordie Powers <gp@gpow.ca>
2022-11-22 14:04:36 -0500

    add catppuccin theme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,6 +70,7 @@ return packer.startup(function(use)
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
   use { "shaunsingh/oxocarbon.nvim", commit = "bb59d8689820a0b5f1cf9508ebc0d429b15348d1", run = "./install.sh"}
+  use { "catppuccin/nvim", as = "catppuccin"}
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
