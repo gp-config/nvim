e82e84d2d664ab54e10930a8f8e6f1f9c6ba5cad
Geordie Powers <gp@gpow.ca>
2022-11-22 14:02:24 -0500

    change theme config

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,8 @@
-local colorscheme = "tokyonight-night"
+-- vim.o.background = "dark" -- loads dark version for some themes
+vim.o.background = "light" -- loads light version for some themes
+
+-- local colorscheme = "tokyonight-night"
+local colorscheme = "oxocarbon"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
