cafcc881510fe497e8d0b3066dc7d937fdcf3198
Geordie Powers <gp@gpow.ca>
2022-11-22 14:02:18 -0500

    add oxocarbon theme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -69,6 +69,7 @@ return packer.startup(function(use)
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
+  use { "shaunsingh/oxocarbon.nvim", commit = "bb59d8689820a0b5f1cf9508ebc0d429b15348d1", run = "./install.sh"}
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
@@ -79,6 +80,7 @@ return packer.startup(function(use)
 
   -- snippets
   -- NOTE:GP: all snippet plugins have been removed for now
+
 
   -- LSP
   -- use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
