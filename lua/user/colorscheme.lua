d296da1e22665966b8796d213d381e65227f49d9
Geordie Powers <gp@gpow.ca>
2022-12-21 11:18:29 -0500

    colors

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,8 @@
 -- BACKGROUND COLOR
 -- automatically chooses a light/dark version for some themes
 --
--- vim.o.background = "dark"
-vim.o.background = "light"
+vim.o.background = "dark"
+-- vim.o.background = "light"
 
 --
 -- CATPPUCCIN
@@ -18,14 +18,14 @@ vim.o.background = "light"
 --
 -- local colorscheme = "tokyonight" -- base version, uses 'background' option to choose either 'day' or 'storm'
 -- local colorscheme = "tokyonight-day"
--- local colorscheme = "tokyonight-night" -- dark theme, darkest variant
+local colorscheme = "tokyonight-night" -- dark theme, darkest variant
 -- local colorscheme = "tokyonight-storm" -- dark theme, medium variant (default dark)
 -- local colorscheme = "tokyonight-moon" -- dark theme, higher contrast variant
 
 --
 -- MISC
 --
-local colorscheme = "oxocarbon" -- supports light/dark background option
+-- local colorscheme = "oxocarbon" -- supports light/dark background option
 -- local colorscheme = "gruvbox-material" -- support light/dark background option
 -- local colorscheme = "everforest" -- support light/dark background option
 
