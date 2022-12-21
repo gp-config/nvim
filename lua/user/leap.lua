fc6ef7cc74dc9a593082281afc2a00c0f567edf2
Geordie Powers <gp@gpow.ca>
2022-12-21 11:18:27 -0500

    different keybinds for leap

--- lua/user/leap.lua
+++ lua/user/leap.lua
@@ -4,3 +4,10 @@ if not status_ok then
 end
 
 leap.add_default_mappings()
+
+local keymap = vim.keymap.set
+
+keymap("n", "<leader>/", "<Plug>(leap-forward-to)")
+keymap("n", "<leader>?", "<Plug>(leap-backward-to)")
+
+
