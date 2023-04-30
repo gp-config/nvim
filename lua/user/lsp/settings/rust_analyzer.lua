f941cc5106d55a7aeb8d10d61d468a3eec40c037
Geordie Powers <gp@gpow.ca>
2023-04-30 13:08:27 -0400

    remove ssr hydrate from rust-analyzer settings

--- lua/user/lsp/settings/rust_analyzer.lua
+++ lua/user/lsp/settings/rust_analyzer.lua
@@ -4,7 +4,7 @@ return {
     ["rust-analyzer"] = {
       cargo = {
         -- leptos SSR gist - https://gist.github.com/GeordieP/07ad21a714f98deda71a9ea4330caef1
-        features = {"ssr", "hydrate"}
+        -- features = {"ssr", "hydrate"}
       }
     }
   }
