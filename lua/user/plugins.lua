6dae81e36d149d27a5a10887a1494efc2b7be88c
Geordie Powers <gp@gpow.ca>
2023-06-19 22:22:11 -0400

    commit missing fzf native plugin ref and command

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -104,6 +104,7 @@ return packer.startup(function(use)
   
   -- Telescope
   use { "nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
+  use { "nvim-telescope/telescope-fzf-native.nvim", commit = "9bc8237565ded606e6c366a71c64c0af25cd7a50", run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
 
   -- Treesitter
   use { "nvim-treesitter/nvim-treesitter", commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac" }
