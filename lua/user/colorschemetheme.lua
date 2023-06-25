79b64ae14472bb13058530d9c2d6a15ee5e9c6d9
Geordie Powers <gp@gpow.ca>
2023-06-25 18:24:28 -0400

    bones family themes

--- lua/user/colorschemetheme.lua
+++ lua/user/colorschemetheme.lua
@@ -1,43 +1,39 @@
+vim.o.termguicolors = true
+
 -- BACKGROUND COLOR
 -- automatically chooses a light/dark version for some themes
---
-vim.o.background = "dark"
--- vim.o.background = "light"
+-- vim.o.background = "dark"
+vim.o.background = "light"
 
---
--- CATPPUCCIN
--- does not seem to support 'background' option
--- 
--- local colorscheme = "catppuccin-latte" -- light theme (default light)
--- local colorscheme = "catppuccin-mocha" -- dark theme, darkest variant (default dark)
+-- local colorscheme = "catppuccin-latte"     -- light theme (default light bg)
+-- local colorscheme = "catppuccin-mocha"     -- dark theme, darkest variant (default dark bg)
 -- local colorscheme = "catppuccin-macchiato" -- dark theme, medium variant
--- local colorscheme = "catppuccin-frappe" -- dark theme, lightest variant
+-- local colorscheme = "catppuccin-frappe"    -- dark theme, lightest variant
 
---
--- TOKYONIGHT
---
--- local colorscheme = "tokyonight" -- base version, uses 'background' option to choose either 'day' or 'storm'
--- local colorscheme = "tokyonight-day"
--- local colorscheme = "tokyonight-night" -- dark theme, darkest variant
--- local colorscheme = "tokyonight-storm" -- dark theme, medium variant (default dark)
--- local colorscheme = "tokyonight-moon" -- dark theme, higher contrast variant
+-- local colorscheme = "tokyonight"           -- base version, supports light/dark
+-- local colorscheme = "tokyonight-day"       -- light theme (default for light bg)
+-- local colorscheme = "tokyonight-night"     -- dark theme, darkest variant
+-- local colorscheme = "tokyonight-storm"     -- dark theme, medium variant (default for dark bg)
+-- local colorscheme = "tokyonight-moon"      -- dark theme, higher contrast variant
 
---
--- melange
---
--- local colorscheme = "melange"
+-- local colorscheme = "melange"              -- supports light/dark
+-- local colorscheme = "oxocarbon"            -- supports light/dark
+-- local colorscheme = "gruvbox-material"     -- supports light/dark
+-- local colorscheme = "everforest"           -- supports light/dark
 
---
+-- local colorscheme = "zenbones"             -- supports light/dark
+local colorscheme = "tokyobones"           -- supports light/dark -- light/dark both nice for elixir
+-- local colorscheme = "kanagawabones"        -- supports dark       -- nice
+-- local colorscheme = "forestbones"          -- supports light/dark -- dark is nice
+-- local colorscheme = "rosebones"            -- supports light/dark -- rosey colors, dark variant has blue bg
+-- local colorscheme = "vimbones"             -- supports light      -- bright green, it's ok
+-- local colorscheme = "neobones"             -- supports light/dark -- nice -- light is minty, dark is a deep sea blue
+-- local colorscheme = "seoulbones"           -- supports light/dark -- mid -- low contrast, bright colors
+-- local colorscheme = "duckbones"            -- supports dark       -- mid
+-- local colorscheme = "nordbones"            -- supports dark       -- mid
+
 -- GP custom themes
---
-local colorscheme = "oxographene"
-
---
--- MISC
---
--- local colorscheme = "oxocarbon" -- supports light/dark background option
--- local colorscheme = "gruvbox-material" -- support light/dark background option
--- local colorscheme = "everforest" -- support light/dark background option
+-- local colorscheme = "oxographene"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -80,6 +80,7 @@ return packer.startup(function(use)
   use { "sainnhe/everforest", commit = "d855af543410c4047fc03798f5d58ddd07abcf2d" }
   use { "savq/melange-nvim", commit = "f15922543dd70b360335effb61411c05c710b99c" }
   use { "gp-themes/oxographene.nvim" }
+  use { "mcchrish/zenbones.nvim", requires = "rktjmp/lush.nvim" }
 
   -- inline color highlighting for hex codes, rgba, etc
   use { "norcalli/nvim-colorizer.lua", commit = "36c610a9717cc9ec426a07c8e6bf3b3abcb139d6" }
