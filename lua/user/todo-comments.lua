71dca378bf46f9bb8eac1de16d23c4db9477ecff
Geordie Powers <gp@gpow.ca>
2023-12-27 13:30:11 -0500

    add todo comments, but it doesn't seem to be working

---
+++ lua/user/todo-comments.lua
@@ -0,0 +1,17 @@
+local M = {
+  "folke/todo-comments.nvim",
+  event = "VimEnter",
+  commit = "4a6737a8d70fe1ac55c64dfa47fcb189ca431872",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = { "TodoTelescope" },
+}
+
+return M
+
+-- FIX: Testing
+-- TODO: Testing
+-- HACK: Testing
+-- WARN: Testing
+-- PERF: Testing
+-- NOTE: Testing
+-- TEST: Testing
