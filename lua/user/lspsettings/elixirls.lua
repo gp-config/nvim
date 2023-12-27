4c8041e3621e7d62e20d64587d8b03d37d47bec8
Geordie Powers <gp@gpow.ca>
2023-12-27 01:43:39 -0500

    example of passing options to elixir language server

--- lua/user/lspsettings/elixirls.lua
+++ lua/user/lspsettings/elixirls.lua
@@ -1,3 +1,8 @@
 return {
   cmd = { "/home/gp/Applications/elixir-ls-v0.18.0/language_server.sh" },
+  -- settings = {
+  --   elixirLS = {
+  --     projectDir = "some_example_sub_directory/"
+  --   }
+  -- }
 }
