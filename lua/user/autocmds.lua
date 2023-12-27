01de98f176fa64c75bb002c530c78fe556c1ffda
Geordie Powers <gp@gpow.ca>
2023-12-27 13:47:55 -0500

    trying an autocmd to format the current buffer on save

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -60,3 +60,9 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.opt_local.spell = true
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  callback = function()
+    vim.lsp.buf.format()
+  end
+})
