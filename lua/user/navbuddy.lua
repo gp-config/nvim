c355f659fabcdcb75d9e2ec92bdefeb3305bb64d
Geordie Powers <gp@gpow.ca>
2023-12-28 16:01:20 -0500

    fix navbuddy by using the 'config' table key, setting lazy false, and adding lspconfig as a dependency

--- lua/user/navbuddy.lua
+++ lua/user/navbuddy.lua
@@ -1,46 +1,42 @@
 local M = {
   "SmiteshP/nvim-navbuddy",
+  lazy = false,
   dependencies = {
+    "neovim/nvim-lspconfig",
     "SmiteshP/nvim-navic",
-    "MunifTanjim/nui.nvim"
+    "MunifTanjim/nui.nvim",
   },
-  opts = { lsp = { auto_attach = true } }
-}
+  config = function()
+    local navbuddy = require("nvim-navbuddy")
+    local actions = require("nvim-navbuddy.actions")
 
--- NOTE: navbuddy works now, BUT:
--- when this config function is un-commented,
--- the plugin seems to no longer load (Navbuddy is not a recognized editor command).
--- TODO: debug this so we can configure arrow keys as navigation keys.
--- 
--- function M.config()
---   local navbuddy = require("nvim-navbuddy")
---   local actions = require("nvim-navbuddy.actions")
---
---   navbuddy.setup({
---     use_default_mappings = true,
---     mappings = {
---       ["<Down>"] = actions.next_sibling(),
---       ["<Up>"] = actions.previous_sibling(),
---
---       ["<Left>"] = actions.parent(),
---       ["<Right>"] = actions.children(),
---       ["0"] = actions.root(),             -- Move to first panel
---
---       ["v"] = actions.visual_name(),      -- Visual selection of name
---       ["V"] = actions.visual_scope(),     -- Visual selection of scope
---
---       ["d"] = actions.delete(),           -- Delete scope
---
---       ["f"] = actions.fold_create(),      -- Create fold of current scope
---       ["F"] = actions.fold_delete(),      -- Delete fold of current scope
---
---       ["c"] = actions.comment(),          -- Comment out current scope
---       ["<Leader>v"] = actions.vsplit(),   -- Open selected node in a vertical split
---       ["<Leader>sh"] = actions.hsplit(),  -- Open selected node in a horizontal split
---
---       ["g?"] = actions.help(),            -- Open mappings help window
---     }
---   })
--- end
+    navbuddy.setup({
+      lsp = { auto_attach = true },
+      use_default_mappings = true,
+      mappings = {
+        ["<Down>"] = actions.next_sibling(),
+        ["<Up>"] = actions.previous_sibling(),
+
+        ["<Left>"] = actions.parent(),
+        ["<Right>"] = actions.children(),
+        ["0"] = actions.root(),             -- Move to first panel
+
+        ["v"] = actions.visual_name(),      -- Visual selection of name
+        ["V"] = actions.visual_scope(),     -- Visual selection of scope
+
+        ["d"] = actions.delete(),           -- Delete scope
+
+        ["f"] = actions.fold_create(),      -- Create fold of current scope
+        ["F"] = actions.fold_delete(),      -- Delete fold of current scope
+
+        ["c"] = actions.comment(),          -- Comment out current scope
+        ["<Leader>v"] = actions.vsplit(),   -- Open selected node in a vertical split
+        ["<Leader>sh"] = actions.hsplit(),  -- Open selected node in a horizontal split
+
+        ["g?"] = actions.help(),            -- Open mappings help window
+      }
+    })
+  end
+}
 
 return M
