69737be3ee33b23f9cad3330be6859f353f801fc
Geordie Powers <gp@gpow.ca>
2023-12-29 17:37:16 -0500

    change todo comment highlighting to red

--- lua/user/todo-comments.lua
+++ lua/user/todo-comments.lua
@@ -4,7 +4,22 @@ local M = {
   commit = "4a6737a8d70fe1ac55c64dfa47fcb189ca431872",
   dependencies = { "nvim-lua/plenary.nvim" },
   cmd = { "TodoTelescope" },
-  opts = { }
+  opts = {
+    keywords = {
+      FIX = {
+        icon = " ", -- icon used for the sign, and in search results
+        color = "error", -- can be a hex color, or a named color (see below)
+        alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+        -- signs = false, -- configure signs for some keywords individually
+      },
+      TODO = { icon = " ", color = "error" },
+      HACK = { icon = " ", color = "warning" },
+      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+    }
+  }
 }
 
 return M
