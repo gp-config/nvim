75b4f1bdc523064ecf3507fe310da81d5f185e14
Geordie Powers <gp@gpow.ca>
2024-01-15 00:14:42 -0500

    messing with color scheme setup

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,113 +1,123 @@
-require "user.launch"
+-- THEME:
+-- gp :: oxographene
+-- based on oxocarbon, with fewer colors.
+-- dark theme, low-color, high text contrast.
+colorscheme { "gp-themes/oxographene.nvim" }
+-- local active = "oxographene"          -- supports dark
 
-local M = {
-  -- "LunarVim/primer.nvim",
+-- THEME:
+-- Zenbones by mcchrish
+-- a set of minimal, low-color themes with high text contrast.
+-- lots of good, high-focus light and dark options.
+-- colorscheme { "mcchrish/zenbones.nvim", dependencies = { "rktjmp/lush.nvim" } }
+-- -- local active = "zenbones"             -- supports light/dark --
+-- -- local active = "tokyobones"           -- supports light/dark -- light/dark both nice for elixir
+-- -- local active = "kanagawabones"        -- supports dark       -- nice
+-- -- local active = "forestbones" -- supports light/dark -- dark is nice
+-- -- local active = "rosebones"            -- supports light/dark -- rosey colors, dark variant has blue bg
+-- -- local active = "vimbones"             -- supports light      -- bright green, it's ok
+-- -- local active = "neobones"             -- supports light/dark -- nice -- light is minty, dark is a deep sea blue
+-- -- local active = "seoulbones"           -- supports light/dark -- mid -- low contrast, bright colors
+-- -- local active = "duckbones"            -- supports dark       -- mid
+-- -- local active = "nordbones"            -- supports dark       -- mid
 
-  -- GP themes
-  -- Uncomment to pick one
-  --
-  -- "gp-themes/oxographene.nvim",
-  "mcchrish/zenbones.nvim", dependencies = { "rktjmp/lush.nvim" },
-  -- "catppuccin/nvim",
-  -- "folke/tokyonight.nvim",
-  -- "sainnhe/gruvbox-material",
-  -- "savq/melange-nvim",
-  -- "shaunsingh/oxocarbon.nvim",
-  -- "sainnhe/everforest",
-  -- "lunarvim/darkplus.nvim",
-
-  --
-  lazy = false,
-  priority = 1000,
+-- THEME:
+-- Zenbones by mcchrish
+-- a set of minimal, low-color themes with high text contrast.
+-- lots of good, high-focus light and dark options.
+colorscheme {
+  "mcchrish/zenbones.nvim",
+  dependencies = { "rktjmp/lush.nvim" },
+  variants = {
+    -- active = "zenbones"             -- supports light/dark --
+    -- active = "tokyobones"           -- supports light/dark -- light/dark both nice for elixir
+    -- active = "kanagawabones"        -- supports dark       -- nice
+    active = "forestbones", -- supports light/dark -- dark is nice
+    -- active = "rosebones"            -- supports light/dark -- rosey colors, dark variant has blue bg
+    -- active = "vimbones"             -- supports light      -- bright green, it's ok
+    -- active = "neobones"             -- supports light/dark -- nice -- light is minty, dark is a deep sea blue
+    -- active = "seoulbones"           -- supports light/dark -- mid -- low contrast, bright colors
+    -- active = "duckbones"            -- supports dark       -- mid
+    -- active = "nordbones"            -- supports dark       -- mid
+  },
 }
+-- local active = "zenbones"             -- supports light/dark --
+-- local active = "tokyobones"           -- supports light/dark -- light/dark both nice for elixir
+-- local active = "kanagawabones"        -- supports dark       -- nice
+-- local active = "forestbones" -- supports light/dark -- dark is nice
+-- local active = "rosebones"            -- supports light/dark -- rosey colors, dark variant has blue bg
+-- local active = "vimbones"             -- supports light      -- bright green, it's ok
+-- local active = "neobones"             -- supports light/dark -- nice -- light is minty, dark is a deep sea blue
+-- local active = "seoulbones"           -- supports light/dark -- mid -- low contrast, bright colors
+-- local active = "duckbones"            -- supports dark       -- mid
+-- local active = "nordbones"            -- supports dark       -- mid
 
-function M.config()
-  vim.o.termguicolors = true
+-- THEME:
+-- Catppuccin by catppuccin group
+-- Cold background, colorful highlighting with warm pastel colors.
+colorscheme { "catppuccin/nvim" }
+-- local active = "catppuccin-latte"     -- light theme (default light bg)
+-- local active = "catppuccin-mocha"     -- dark theme, darkest variant (default dark bg)
+local active = "catppuccin-macchiato" -- dark theme, medium variant
+-- local active = "catppuccin-frappe"    -- dark theme, lightest variant
 
-  -- THEME:
-  -- Choose LIGHT or DARK!
-  -- 
-  vim.o.background = "dark"
-  -- vim.o.background = "light"
+colorscheme { "gp-themes/oxographene.nvim" }
+-- THEME:
+-- tokyonight by folke
+-- cold background, colorful, bright highlighting with blues and greens.
+colorscheme { "folke/tokyonight.nvim" }
+-- local active = "tokyonight"           -- base version, supports light/dark
+-- local active = "tokyonight-day"       -- light theme (default for light bg)
+-- local active = "tokyonight-night"     -- dark theme, darkest variant
+-- local active = "tokyonight-storm"     -- dark theme, medium variant (default for dark bg)
+-- local active = "tokyonight-moon"      -- dark theme, higher contrast variant
 
-  -- THEME:
-  -- gp :: oxographene
-  -- based on oxocarbon, with fewer colors.
-  -- dark theme, low-color, high text contrast.
-  --
-  -- local colorscheme = "oxographene"          -- supports dark
+-- THEME:
+-- gruvbox material by sainnhe
+-- rustic with vibrant, earthy colors.
+colorscheme { "sainnhe/gruvbox-material" }
+-- local active = "gruvbox-material"     -- supports light/dark
 
-  -- THEME:
-  -- Zenbones by mcchrish
-  -- a set of minimal, low-color themes with high text contrast.
-  -- lots of good, high-focus light and dark options.
-  -- 
-  -- local colorscheme = "zenbones"             -- supports light/dark --
-  -- local colorscheme = "tokyobones"           -- supports light/dark -- light/dark both nice for elixir
-  -- local colorscheme = "kanagawabones"        -- supports dark       -- nice
-  local colorscheme = "forestbones"          -- supports light/dark -- dark is nice
-  -- local colorscheme = "rosebones"            -- supports light/dark -- rosey colors, dark variant has blue bg
-  -- local colorscheme = "vimbones"             -- supports light      -- bright green, it's ok
-  -- local colorscheme = "neobones"             -- supports light/dark -- nice -- light is minty, dark is a deep sea blue
-  -- local colorscheme = "seoulbones"           -- supports light/dark -- mid -- low contrast, bright colors
-  -- local colorscheme = "duckbones"            -- supports dark       -- mid
-  -- local colorscheme = "nordbones"            -- supports dark       -- mid
-  --
+-- THEME:
+-- melange by savq
+-- rustic with pastel colors. similar to gruvbox.
+colorscheme { "savq/melange-nvim" }
+-- local active = "melange"              -- supports light/dark
 
-  -- THEME:
-  -- Catppuccin by catppuccin group
-  -- Cold background, colorful highlighting with warm pastel colors.
-  --
-  -- local colorscheme = "catppuccin-latte"     -- light theme (default light bg)
-  -- local colorscheme = "catppuccin-mocha"     -- dark theme, darkest variant (default dark bg)
-  -- local colorscheme = "catppuccin-macchiato" -- dark theme, medium variant
-  -- local colorscheme = "catppuccin-frappe"    -- dark theme, lightest variant
+-- THEME:
+-- oxocarbon by shaunsingh
+-- bright sci-fi colors; primarily cyan and magenta
+colorscheme { "shaunsingh/oxocarbon.nvim" }
+-- local active = "oxocarbon"            -- supports light/dark
 
-  -- THEME:
-  -- tokyonight by folke
-  -- cold background, colorful, bright highlighting with blues and greens.
-  --
-  -- local colorscheme = "tokyonight"           -- base version, supports light/dark
-  -- local colorscheme = "tokyonight-day"       -- light theme (default for light bg)
-  -- local colorscheme = "tokyonight-night"     -- dark theme, darkest variant
-  -- local colorscheme = "tokyonight-storm"     -- dark theme, medium variant (default for dark bg)
-  -- local colorscheme = "tokyonight-moon"      -- dark theme, higher contrast variant
+-- THEME:
+-- everforest sainnhe
+-- cold background, earthy colors similar to gruvbox. low saturation.
+colorscheme { "sainnhe/everforest" }
+-- local active = "everforest"           -- supports light/dark
 
-  -- THEME:
-  -- gruvbox material by sainnhe
-  -- rustic with vibrant, earthy colors.
-  --
-  -- local colorscheme = "gruvbox-material"     -- supports light/dark
+-- THEME:
+-- darkplus by the lunarvim group
+-- inspired by the VS Code default theme.
+-- gray background, lots of cooler colors.
+colorscheme { "lunarvim/darkplus.nvim" }
+-- local active = "darkplus"            -- supports light/dark
 
-  -- THEME:
-  -- melange by savq
-  -- rustic with pastel colors. similar to gruvbox.
-  --
-  -- local colorscheme = "melange"              -- supports light/dark
+-- THEME:
+-- Choose LIGHT or DARK!
+--
+vim.o.background = "dark"
+-- vim.o.background = "light"
+vim.o.termguicolors = true
 
-  -- THEME:
-  -- everforest sainnhe
-  -- cold background, earthy colors similar to gruvbox. low saturation.
-  --
-  -- local colorscheme = "everforest"           -- supports light/dark
-
-  -- THEME:
-  -- oxocarbon by shaunsingh
-  -- bright sci-fi colors; primarily cyan and magenta
-  --
-  -- local colorscheme = "oxocarbon"            -- supports light/dark
-
-  -- THEME:
-  -- darkplus by the lunarvim group
-  -- inspired by the VS Code default theme.
-  -- gray background, lots of cooler colors.
-  --
-  -- local colorscheme = "darkplus"            -- supports light/dark
-
-  local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-  if not status_ok then
-    return
-  end
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. active)
+if not status_ok then
+  return
 end
 
-return M
+-- local M = { opts = {} }
+--
+-- function M.config()
+-- end
+--
+-- return M
--- lua/user/launch.lua
+++ lua/user/launch.lua
@@ -3,3 +3,12 @@ LAZY_PLUGIN_SPEC = {}
 function spec(item)
   table.insert(LAZY_PLUGIN_SPEC, { import = item })
 end
+
+function colorscheme(item)
+  local active_variant = item.variants.active
+  item.variants = nil
+
+  table.insert(LAZY_PLUGIN_SPEC, item)
+
+  return active_variant
+end
