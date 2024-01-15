98b10e19db77537fb1394778ebcf409424f766cf
Geordie Powers <gp@gpow.ca>
2024-01-14 23:22:21 -0500

    formatting i think

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -210,13 +210,13 @@ function M.config()
       ghost_text = false,
     },
     enabled = function()
-        local context = require("cmp.config.context")
-        local disabled = false
-        disabled = disabled or (vim.api.nvim_buf_get_option(0, "buftype") == "prompt")
-        disabled = disabled or (vim.fn.reg_recording() ~= "")
-        disabled = disabled or (vim.fn.reg_executing() ~= "")
-        disabled = disabled or context.in_treesitter_capture("comment")
-        return not disabled
+      local context = require "cmp.config.context"
+      local disabled = false
+      disabled = disabled or (vim.api.nvim_buf_get_option(0, "buftype") == "prompt")
+      disabled = disabled or (vim.fn.reg_recording() ~= "")
+      disabled = disabled or (vim.fn.reg_executing() ~= "")
+      disabled = disabled or context.in_treesitter_capture "comment"
+      return not disabled
     end,
   }
 
