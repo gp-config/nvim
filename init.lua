b13862fea9d8455959d1a11f607eb5dee939483b
Geordie Powers <gp@gpow.ca>
2024-02-10 17:51:46 -0500

    update firenvim description

--- init.lua
+++ init.lua
@@ -44,7 +44,7 @@ spec "user.twilight"                      -- twilight: lowlight code that's far 
 spec "user.obsidian"                      -- obsidian: support obsidian.md vaults
 spec "user.persistence"                   -- persistence: auto-save the session on nvim quit -- use this alongside an autocmd to re-open the last session for the current folder on startup!
 spec "user.nvim-treesitter-textobjects"   -- nvim-treesitter-textobjects: select, move, or swap treesitter nodes
-spec "user.firenvim"
+spec "user.firenvim"                      -- firenvim: use neovim to edit text boxes on websites (requires a browser extension)
 spec "user.telescope-menu"                -- telescope-menu: define custom menus, displayed by telescope
 --
 require "user.lazy"                       -- lazy: plugin management framework - installs every `spec` above
