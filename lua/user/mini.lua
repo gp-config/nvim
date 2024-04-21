24a5346c23b02503c4e8c6d904f6225fd8b0d0c9
Geordie Powers <gp@gpow.ca>
2024-04-20 21:23:43 -0400

    enable mini.align

--- lua/user/mini.lua
+++ lua/user/mini.lua
@@ -8,6 +8,10 @@ function M.config()
   -- NOTE: set up desired mini.nvim modules here.
   -- LINK: list of available modules (w/ commit SHA): https://github.com/echasnovski/mini.nvim/tree/d0b297ab233a57bed00ab69bd66135dee111967f
 
+  -- DESC: mini-align
+  --       fancy code alignment
+  require("mini.align").setup()
+
   -- TODO: add the default mini-bracketed key binds to which-key
   -- DESC: mini-bracketed
   --       various ways to navigate forwards/backwards using square brackets as a leader key
