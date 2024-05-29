295a2d77daded3faf38cf3caaa0dc9646aef84c5
Geordie Powers <gp@gpow.ca>
2024-05-29 12:13:12 -0400

    install all color schemes at once
    just putting them all in dependencies for now

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -39,25 +39,20 @@ local function define_language_colors(opts)
 end
 
 local M = {
-  -- "LunarVim/primer.nvim",
+  "LunarVim/primer.nvim",
+  dependencies = {
+    "rktjmp/lush.nvim",
+    "gp-themes/oxographene.nvim",
+    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
+    "sainnhe/gruvbox-material",
+    "savq/melange-nvim",
+    "shaunsingh/oxocarbon.nvim",
+    "sainnhe/everforest",
+    "lunarvim/darkplus.nvim",
+    "mcchrish/zenbones.nvim",
+  },
 
-  -- GP themes
-  -- Uncomment to pick one
-  --
-  -- "gp-themes/oxographene.nvim",
-  -- "catppuccin/nvim",
-  -- "folke/tokyonight.nvim",
-  -- "sainnhe/gruvbox-material",
-  -- "savq/melange-nvim",
-  -- "shaunsingh/oxocarbon.nvim",
-  -- "sainnhe/everforest",
-  -- "lunarvim/darkplus.nvim",
-  --
-  "mcchrish/zenbones.nvim",
-  dependencies = { "rktjmp/lush.nvim" },
-  --
-
-  --
   lazy = false,
   priority = 1000,
 }
@@ -68,8 +63,8 @@ function M.config()
   -- THEME:
   -- Choose LIGHT or DARK!
   --
-  vim.o.background = "dark"
-  -- vim.o.background = "light"
+  -- vim.o.background = "dark"
+  vim.o.background = "light"
 
   -- THEME:
   -- gp :: oxographene
@@ -83,17 +78,17 @@ function M.config()
   -- a set of minimal, low-color themes with high text contrast.
   -- lots of good, high-focus light and dark options.
   --
-  -- local colorscheme = "randombones"          -- picks a random zenbones theme --
-  -- local colorscheme = "zenbones"             -- supports light/dark --
+  local colorscheme = "zenbones"             -- supports light/dark --
   -- local colorscheme = "tokyobones"           -- supports light/dark -- light/dark both nice for elixir
   -- local colorscheme = "kanagawabones"        -- supports dark       -- nice
   -- local colorscheme = "forestbones"          -- supports light/dark -- dark is nice
-  local colorscheme = "rosebones"             -- supports light/dark -- rosey colors, dark variant has blue bg
+  -- local colorscheme = "rosebones"             -- supports light/dark -- rosey colors, dark variant has blue bg
   -- local colorscheme = "vimbones"             -- supports light      -- bright green, it's ok
   -- local colorscheme = "neobones"             -- supports light/dark -- nice -- light is minty, dark is a deep sea blue
   -- local colorscheme = "seoulbones"           -- supports light/dark -- mid -- low contrast, bright colors
   -- local colorscheme = "duckbones"            -- supports dark       -- kind anice for HTML
   -- local colorscheme = "nordbones"            -- supports dark       -- mid
+  -- local colorscheme = "randombones"          -- picks a random zenbones theme --
   --
 
   -- THEME:
